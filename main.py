cc41ecb2a4b6d72eb7ab1c03183a04bffc7cf70e
henryxqy <33465711+henryxqy@users.noreply.github.com>
2026-04-23 13:28:15 +0800

    Change server URL in add_server_time function
    Updated the server URL in the add_server_time function.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 import time
 from playwright.sync_api import sync_playwright, Cookie, TimeoutError as PlaywrightTimeoutError
 
-def add_server_time(server_url="https://dash.icehost.pl/server/2920225f"):
+def add_server_time(server_url="https://dash.icehost.pl/server/4385d863"):
     """
     优先使用 REMEMBER_WEB_COOKIE 进行会话登录，如果不存在则回退到邮箱密码登录。
     此函数设计为每次GitHub Actions运行时执行一次。
